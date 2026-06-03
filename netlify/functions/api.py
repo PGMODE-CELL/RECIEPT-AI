10a8bc7a140933d65f736280968d6ddc4bd9e8c0
diavolodellamorte-tech <diavolodellamorte@gmail.com>
2026-06-04 00:03:41 +0530

    feat: Deploy backend as Netlify Function

---
+++ netlify/functions/api.py
@@ -0,0 +1,10 @@
+﻿import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", "huh", "backend"))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", ".."))
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from mangum import Mangum
+from huh.backend.main import app
+handler = Mangum(app, lifespan="off")
