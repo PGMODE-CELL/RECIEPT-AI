0e03d551d2f207522a242f63e65c8a914b7864bc
diavolodellamorte-tech <diavolodellamorte@gmail.com>
2026-06-03 23:51:42 +0530

    live: https://117a93dc538e2c.lhr.life

---
+++ api/index.py
@@ -0,0 +1,7 @@
+﻿import sys, os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "huh", "backend"))
+
+from mangum import Mangum
+from huh.backend.main import app
+handler = Mangum(app)
